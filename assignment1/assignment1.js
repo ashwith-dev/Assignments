063285387a92f68aae7f06942e9e4dbaf2e074ac
ashwith-dev <ashwiththatipally@gmail.com>
2025-11-15 14:50:36 +0530

    changes

--- assignment1/assignment1.js
+++ assignment1/assignment1.js
@@ -1,10 +1,11 @@
 function calculateSum() {
-      const n = document.getElementById('numInput').value;
+      let n = document.getElementById('numInput').value;
       if (n < 1) {
         document.getElementById('result').innerText = "Please enter a valid number.";
         return;
       }
-
-      let sum = (n * (n + 1)) / 2; 
+      
+       const sum = (n * (n + 1)) / 2;
       document.getElementById('result').innerText = `Sum is ${sum}`;
+      
     }
