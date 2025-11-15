5c3d0191bfb3b9e6536152b630b55f695b01db52
ashwith-dev <ashwiththatipally@gmail.com>
2025-11-15 13:45:05 +0530

    3rd assignment java

---
+++ assignment3/assignment3.js
@@ -0,0 +1,16 @@
+ function pattern() {
+      let n = document.getElementById('n').value;
+      if (!n || n < 1) {
+        document.getElementById('result').innerText = "Enter a positive integer.";
+        return;
+      }
+       if (n > 100) {
+    document.getElementById('result').innerText = "Number too large! Enter a number up to 100.";
+    return;
+  }
+      let result = "";
+      for (let i = 1; i <= n; i++) {
+        result += "*".repeat(i) + "\n";
+      }
+      document.getElementById('result').innerText = result;
+    }
